a1dc0270309f166989a56b79951f358f7c3bb1b6
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-20 12:35:18 -0700

    Set up hardcoded components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/ Header';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { IncomeExpenseBox } from './components/IncomeExpenseBox';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenseBox />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
   );
 }
 
